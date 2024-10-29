9a8735ad1028cc1688cd08c5a9e7f603c9654167
tesia102 <koworihime@gmail.com>
2024-10-29 13:53:00 +0900

    20241029_학원 마지막날

---
+++ Assets/Scripts/Player/Command/CommandList.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandList : ICommandRule
+{
+    //public bool IsMatch(IEnumerable<I>)
+    public void ExcuteAction()
+    {
+        
+    }
+}
--- Assets/Scripts/Player/Command/ICommandRule.cs
+++ Assets/Scripts/Player/Command/ICommandRule.cs
@@ -1,3 +1,4 @@
 public interface ICommandRule
 {
+    void ExcuteAction();
 }
